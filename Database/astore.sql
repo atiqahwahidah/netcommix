a96f37ae8283936a3a3fde2ead5fe4d47f3cfe17
atiqahwahidah <2020483286@student.uitm.edu.my>
2023-01-26 11:05:10 +0800

    initial commit

---
+++ Database/astore.sql
@@ -0,0 +1,330 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2023 at 04:16 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `astore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `AddressID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `FullName` varchar(50) NOT NULL,
+  `StreetAddress` varchar(255) NOT NULL,
+  `PostCode` varchar(5) NOT NULL,
+  `City` varchar(28) NOT NULL,
+  `Country` varchar(28) NOT NULL,
+  `Phone` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `addresses`
+--
+
+INSERT INTO `addresses` (`AddressID`, `UserID`, `FullName`, `StreetAddress`, `PostCode`, `City`, `Country`, `Phone`) VALUES
+(1, 1, 'Admin', 'Vanha Maantie 6', '02650', 'Espoo', 'Finland', '0123456789'),
+(2, 2, 'Anh Pham', 'Vanha Maantie 8', '02650', 'Helsinki', 'Finland', '012 345 6787'),
+(3, 3, 'adones evangelista', 'Purok1, Brgy.Tan-awan', '6111', 'Kabankalan City', 'Philippines', '09120119912'),
+(4, 4, 'Nuna Zkrn', 'lot 67, taman nuna', '22000', 'jerteh', 'terengganu', '0162782563');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` int(11) NOT NULL,
+  `CategoryName` varchar(58) NOT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `CategorySlug` varchar(68) NOT NULL,
+  `Image` varchar(58) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`CategoryID`, `CategoryName`, `Description`, `CategorySlug`, `Image`) VALUES
+(3, 'Fiction', 'Fiction Books.', 'Fiction', 'computers.png'),
+(4, 'Non-Fiction ', 'Non-Fiction Books.', 'Non-Fiction', 'game-console.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `MessageID` int(11) NOT NULL,
+  `FullName` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Subject` varchar(128) DEFAULT NULL,
+  `Content` varchar(158) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order details`
+--
+
+CREATE TABLE `order details` (
+  `OrderID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quantity` smallint(2) NOT NULL DEFAULT 1,
+  `Total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order details`
+--
+
+INSERT INTO `order details` (`OrderID`, `ProductID`, `Quantity`, `Total`) VALUES
+(2, 7, 1, '85.00'),
+(3, 5, 1, '55.00'),
+(3, 8, 1, '110.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `AddressID` int(11) NOT NULL,
+  `SubTotal` decimal(10,2) DEFAULT NULL,
+  `Discount` decimal(10,2) DEFAULT NULL,
+  `ShippingFee` decimal(10,2) DEFAULT NULL,
+  `Total` decimal(10,2) DEFAULT NULL,
+  `OrderDate` datetime DEFAULT NULL,
+  `Status` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `UserID`, `AddressID`, `SubTotal`, `Discount`, `ShippingFee`, `Total`, `OrderDate`, `Status`) VALUES
+(1, 3, 3, '850.52', '0.00', '0.00', '850.52', '2022-04-06 16:51:10', 'Order Received'),
+(2, 4, 4, '85.00', '0.00', '0.00', '85.00', '2023-01-24 19:43:03', 'Order Received'),
+(3, 4, 4, '165.00', '0.00', '0.00', '165.00', '2023-01-25 12:06:06', 'Order Received');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(40) NOT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `ProductPrice` decimal(10,2) DEFAULT 0.00,
+  `UnitsInStock` smallint(5) DEFAULT 0,
+  `Description` varchar(255) NOT NULL,
+  `ManufactureYear` smallint(5) NOT NULL,
+  `Image` varchar(50) NOT NULL,
+  `ProductSlug` varchar(50) NOT NULL,
+  `Feature` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `CategoryID`, `ProductPrice`, `UnitsInStock`, `Description`, `ManufactureYear`, `Image`, `ProductSlug`, `Feature`) VALUES
+(5, 'The Dust That Falls From Dreams', 3, '55.00', 7, 'A sumptuous, sweeping, powerfully moving work of fiction, The Dust That Falls from Dreams is a story of profound loss and indelible hope. ', 2015, '5.png', 'The-Dust-That-Falls-From-Dreams', 1),
+(6, 'The Mark and The Void', 3, '90.00', 6, 'The followup to Skippy Dies is a tricksy post-crash comedy set among bankers and chancers in a Dublin on the verge of collapse.\r\n', 4097, '6.png', 'The-Mark-and-The-Void', 1),
+(7, 'The Snakehead', 4, '85.00', 11, '\'The Snakehead\' is centred around the disastrous running aground of the Golden Venture, a ship loaded with illegal Chinese immigrants, in Queens, New York.', 2009, '7.png', 'The-Snakehead', 1),
+(8, 'The Psychology of Stupidity', 4, '110.00', 13, 'In The Psychology of Stupidity, some of the world\'s leading psychologists and thinkers—including a Nobel Prize winner and bestselling authors—will show you.', 2018, '8.png', 'The-Psychology-of-Stupidity', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `FullName` varchar(50) NOT NULL,
+  `StreetAddress` varchar(255) NOT NULL,
+  `PostCode` varchar(5) NOT NULL,
+  `City` varchar(28) NOT NULL,
+  `Country` varchar(28) NOT NULL,
+  `Phone` varchar(12) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Username` varchar(28) DEFAULT NULL,
+  `Password` varchar(158) DEFAULT NULL,
+  `Admin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `FullName`, `StreetAddress`, `PostCode`, `City`, `Country`, `Phone`, `Email`, `Username`, `Password`, `Admin`) VALUES
+(1, 'Admin', 'Purok 1, Brgy.Enclaro', '6107', 'Binalbagan City', 'Phippines', '0912011191', 'admin@gmail.com', 'admin', '$2a$10$mpJCYlSr762SwQVzdLwxR.KgRuWEHA2NzUanxxG/nxEStDRcRBbB6', 1),
+(2, 'Jude Suares', 'Brgy.tooy', '6108', 'Himamaylan City', 'Philippines', '09123456732', 'jude@gmail.com', 'jude', '$2a$10$TsD7IW0m1g/57C931nC7R.FjwXw9i0tAbJZk7u4Uk0gDoneR9yBim', 0),
+(3, 'adones evangelista', 'Purok1, Brgy.Tan-awan', '6111', 'Kabankalan City', 'Philippines', '09120119912', 'adones@gmail.com', 'adones', '$2a$10$iuJPC5hx2s8DUwvjZAq1ZOmNzBKMO7zFTn7Tq9roUr9HFKGfFGToe', 0),
+(4, 'Nuna Zkrn', 'lot 67, taman nuna', '22000', 'jerteh', 'terengganu', '0162782563', 'nunazkrnent@gmail.com', 'nunazkrn', '$2a$10$Fxt8.KgL4rIbNocXdF.fJ..UBLCid4MlFKYL.c9AU7jLd2KaIXhYi', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`AddressID`),
+  ADD KEY `FK_Users_UserID` (`UserID`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`),
+  ADD KEY `CategoryName` (`CategoryName`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`MessageID`);
+
+--
+-- Indexes for table `order details`
+--
+ALTER TABLE `order details`
+  ADD PRIMARY KEY (`OrderID`,`ProductID`),
+  ADD KEY `FK_Order_Details_Products` (`ProductID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `FK_Orders_Users` (`UserID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `FK_Products_Categories` (`CategoryID`),
+  ADD KEY `ProductName` (`ProductName`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `Username` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `AddressID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `FK_Users_UserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order details`
+--
+ALTER TABLE `order details`
+  ADD CONSTRAINT `FK_Order_Details_Orders` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_Order_Details_Products` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_Orders_Users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_Products_Categories` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
